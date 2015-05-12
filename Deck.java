2d7547c79e0bd502a0ea236ccd2d15522e8ca05a
Local Student <student@406-imac16.tahomasd.private>
2015-05-12 09:48:34 -0700

    now the deck class works

--- Deck.java
+++ Deck.java
@@ -7,6 +7,20 @@ public class Deck {
    public Deck(){
       deck = new ArrayList<Card>();
       //TODO: Add a card (2-14) of each suit (h,s,d,c)
+      String suit = "h";
+      for(int j = 0; j < 4; j++)
+      {
+        for(int i = 2; i < 15; i++)
+        {
+          deck.add(new Card(i, suit));
+        }
+        if(j == 0)
+          suit = "s";
+        if(j == 1)
+          suit = "d";
+        if(j == 2)
+          suit = "c";
+      }
    }
 
    public Card remove(){
